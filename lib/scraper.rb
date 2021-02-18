a5b1df56dad0df587d71ba98df8b095154d8612e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-18 21:53:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,11 @@
-require 'nokogiri'
 require 'open-uri'
-
-html = open("https://flatironschool.com/")
+require 'nokogiri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
-
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css("#-3c239880-48cf-5ce7-84d0-578546d87586.inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course|
-  puts course.text.strip
+  puts course.text.stripe
+end
 end
