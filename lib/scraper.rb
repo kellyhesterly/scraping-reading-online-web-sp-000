bb032db3b3042afa21f01ec87dbcfd5cb7a8cb90
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-18 22:19:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,3 +3,4 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586.inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
